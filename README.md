8871fb0b3fe1ce3395ba302dbde48c0c0219bee0
Sam G <46730010+sam-g99@users.noreply.github.com>
2020-05-10 11:45:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Range Slider
+
+Experimenting with making custom range sliders.
+
+Work in progress.
---
+++ main.js
@@ -0,0 +1,74 @@
+const track = document.getElementById('track');
+const progress = document.getElementById('progress');
+const thumb = document.getElementById('thumb');
+
+function rangeSlider(track, thumb, progress) {
+	this.percentage = 0;
+
+	/* Thumb accounted for in track length */
+	const trackLength = track.offsetHeight - thumb.offsetHeight;
+
+	const drag = (e) => {
+		const scrollDistance =
+			window.pageYOffset ||
+			(
+				document.documentElement ||
+				document.body.parentNode ||
+				document.body
+			).scrollTop;
+
+		const sliderDisFromTopOfPage =
+			window.pageYOffset + track.getBoundingClientRect().top;
+
+		const thumbDisFromTrackBottom = trackLength - thumb.offsetTop;
+
+		const percentagePositionToChange =
+			-(e.clientY - sliderDisFromTopOfPage) +
+			track.offsetHeight +
+			scrollDistance;
+
+		// Align the thumb more with the cursor
+		const halfOfThumb = thumb.offsetHeight / 2;
+
+		const bottomDistance =
+			(track.offsetHeight * percentagePositionToChange) / 100 -
+			halfOfThumb;
+
+		let finalBottomDistance;
+		let finalPercentage;
+		// Make sure it doesn't go off track
+		if (bottomDistance <= 0) {
+			finalBottomDistance = 0;
+			finalPercentage = 0;
+		} else if (bottomDistance >= track.offsetHeight) {
+			finalBottomDistance = trackLength;
+			finalPercentage = 100;
+		} else {
+			if (bottomDistance > 0 && bottomDistance <= trackLength) {
+				finalBottomDistance = bottomDistance;
+				finalPercentage = percentagePositionToChange;
+			}
+		}
+
+		thumb.style.bottom = `${finalBottomDistance}px`;
+		progress.style.height = `${finalPercentage}px`;
+
+		// trackLength is the max distance the thumb can go
+		this.percentage = finalPercentage;
+	};
+
+	thumb.addEventListener('mousedown', (e) => {
+		document.onmousemove = drag;
+	});
+
+	document.addEventListener('mouseup', () => {
+		document.onmousemove = null;
+	});
+
+	track.addEventListener('mousedown', (e) => {
+		drag(e);
+		document.onmousemove = drag;
+	});
+}
+
+const rangeSlider1 = new rangeSlider(track, thumb, progress);
