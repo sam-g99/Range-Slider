00921cbd249f0d70275402cec7959857d3da725d
Sam G <46730010+sam-g99@users.noreply.github.com>
2020-05-10 13:48:55 -0400

    more accurate and display percentage

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ const track = document.getElementById('track');
 const progress = document.getElementById('progress');
 const thumb = document.getElementById('thumb');
 
+const event = new Event('slider');
 function rangeSlider(track, thumb, progress) {
 	this.percentage = 0;
 
 	/* Thumb accounted for in track length */
 	const trackLength = track.offsetHeight - thumb.offsetHeight;
+
+	// const realTrackRange = trackLength - thumb.offsetHeight;
+
+	// const thumbMiddle = thumb.offsetHeight / 2;
+
+	// const thumbsCenterPosition =
+	// debugger;
 
 	const drag = (e) => {
 		const scrollDistance =
@@ -22,15 +30,13 @@ function rangeSlider(track, thumb, progress) {
 			track.getBoundingClientRect().top -
 			scrollDistance;
 
+		// Align the thumb more with the cursor
+		const halfOfThumb = thumb.offsetHeight / 2;
 		const percentagePositionToChange =
 			-(e.clientY - sliderDisFromTopOfPage) + track.offsetHeight;
 
-		// Align the thumb more with the cursor
-		const halfOfThumb = thumb.offsetHeight / 2;
-
-		const bottomDistance =
-			(track.offsetHeight * percentagePositionToChange) / 100 -
-			halfOfThumb;
+		const bottomDistance = (trackLength * percentagePositionToChange) / 100;
+		console.log('changeeee', percentagePositionToChange, bottomDistance);
 
 		let finalBottomDistance;
 		let finalPercentage;
@@ -45,14 +51,20 @@ function rangeSlider(track, thumb, progress) {
 		} else {
 			if (bottomDistance > 0 && bottomDistance <= trackLength) {
 				finalBottomDistance = bottomDistance;
+			}
+			const p = percentagePositionToChange;
+			if (p <= 100 && p >= 0) {
 				finalPercentage = percentagePositionToChange;
+			} else {
+				finalPercentage = 100;
 			}
 		}
-
 		thumb.style.bottom = `${finalBottomDistance}px`;
-		progress.style.height = `${finalPercentage}px`;
+		progress.style.height = `${finalPercentage}%`;
 
 		this.percentage = finalPercentage;
+
+		window.dispatchEvent(event);
 	};
 
 	thumb.addEventListener('mousedown', (e) => {
@@ -70,3 +82,7 @@ function rangeSlider(track, thumb, progress) {
 }
 
 const rangeSlider1 = new rangeSlider(track, thumb, progress);
+
+window.addEventListener('slider', (e) => {
+	document.getElementById('percentage').textContent = rangeSlider1.percentage;
+});
